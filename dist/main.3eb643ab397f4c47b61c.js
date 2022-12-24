e3f5ba37c821626898441f7a39051f40def1ea0a
Justin Mach <bachmach24k@gmail.com>
2022-12-24 12:03:39 -0600

    Add: typescript configs

---
+++ dist/main.3eb643ab397f4c47b61c.js
@@ -0,0 +1,11 @@
+/******/ (() => { // webpackBootstrap
+/******/ 	"use strict";
+var __webpack_exports__ = {};
+/*!**********************!*\
+  !*** ./src/index.ts ***!
+  \**********************/
+
+
+/******/ })()
+;
+//# sourceMappingURL=main.3eb643ab397f4c47b61c.js.map
---
+++ webpack.config.js
@@ -0,0 +1,56 @@
+const path = require("path");
+const htmlWebpackPlugin = require("html-webpack-plugin");
+
+module.exports = {
+  mode: "development",
+  entry: "./src/index.ts",
+  output: {
+    filename: "[name].[contenthash].js",
+    path: path.resolve(__dirname, "dist"),
+    clean: true,
+    assetModuleFilename: "[name][ext]",
+  },
+  devtool: "source-map",
+  module: {
+    rules: [
+      {
+        test: /\.tsx?$/,
+        use: "ts-loader",
+        exclude: /node_modules/,
+      },
+      {
+        test: /\.scss/,
+        use: ["style-loader", "css-loader", "sass-loader"],
+      },
+      {
+        test: /\.(png|svg|jpg|jpeg|gif)$/i,
+        type: "asset/resource",
+      },
+      {
+        test: /\.(png|woff|woff2|eot|ttf|svg)$/,
+        use: "url-loader?limit=100000",
+      },
+    ],
+  },
+  resolve: {
+    extensions: [".tsx", ".ts", ".js"],
+  },
+  plugins: [
+    // eslint-disable-next-line new-cap
+    new htmlWebpackPlugin({
+      title: "Dyno",
+      filename: "index.html",
+      template: "./src/template.html",
+    }),
+  ],
+  devServer: {
+    static: {
+      directory: path.join(__dirname, "dist"),
+    },
+    port: 3000,
+    open: true,
+    hot: true,
+    compress: true,
+    historyApiFallback: true,
+  },
+};
