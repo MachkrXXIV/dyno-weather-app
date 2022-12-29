e38286a497947de3e1e1bbda9654b56aec6f5447
Justin Mach <bachmach24k@gmail.com>
2022-12-29 08:31:16 -0600

    Fix: search-bar reloading page on submit

--- src/index.ts
+++ src/index.ts
@@ -1,2 +1,16 @@
 import "./styles/reset.scss";
 import "./styles/style.scss";
+import { search, getCoords, buildCoords } from "./modules/weather";
+
+const searchBarForm = document.querySelector(".search-bar");
+
+const initialLoad = function initialPageLoad() {
+  const city = search();
+  const coordinateEndpoint = buildCoords(city);
+  getCoords(coordinateEndpoint);
+};
+
+searchBarForm?.addEventListener("submit", (e) => {
+  e.preventDefault();
+  initialLoad();
+});
--- webpack.config.js
+++ webpack.config.js
@@ -28,7 +28,6 @@ module.exports = {
       },
       {
         test: /\.(png|woff|woff2|eot|ttf|svg)$/,
-        // use: "url-loader?limit=100000",
         type: "asset/resource",
       },
     ],
